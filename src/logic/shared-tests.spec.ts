bff7444de80a1ec5a4406eb416afb293d916802a
Sergei Maertens <sergei@maykinmedia.nl>
2026-03-16 16:32:31 +0100

    :white_check_mark: [open-formulieren/open-forms#6037] Set up tests with shared test collection
    So that we can align backend and frontend behaviour.

---
+++ src/logic/shared-tests.spec.ts
@@ -0,0 +1,23 @@
+import type {JSONObject, JSONValue} from '@open-formulieren/types';
+
+import evaluate from '.';
+import sharedTests from '../../org-dotgithub/json-logic/custom-operators.json';
+
+interface CasesGroup {
+  name: string;
+  // expression / input data / result
+  cases: [JSONObject, JSONObject, JSONValue][];
+}
+
+for (const {name, cases} of sharedTests as CasesGroup[]) {
+  describe(name, () => {
+    test.each(cases)(
+      `expression: %o, input: %o`,
+      (expression: JSONObject, inputData: JSONObject, expectedResult: JSONValue) => {
+        const result = evaluate(expression, inputData);
+
+        expect(result).toEqual(expectedResult);
+      }
+    );
+  });
+}
